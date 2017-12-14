85eeb8d004879763f03aa61db533bd35724f64d3
PBM42 <PBM764927@gmail.com>
2017-12-14 10:13:52 +0100

    docs: document functions

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,26 @@ export * from './data.structures';
 
 export type IQuery = IAtomicQuery | IGoalQuery | IProviderQuery;
 
+/**
+ * Construct query's `name` property.
+ * @param nameStr if not set, use `'query'`.
+ */
 export const name = (nameStr?: string): Record<'name', string> => ({ name: nameStr || 'query' });
+
+/**
+ * Construct query's `kind` property.
+ * @param kindQK if not set, use `Atomic` kind.
+ */
 export const kind = (kindQK?: QueryKind): Record<'kind', QueryKind> => ({
   kind: kindQK != null ? kindQK : QueryKind.Atomic,
 });
+
+/**
+ * Construct query's `id` property.
+ * @param idNb if not set, use `42`.
+ */
 export const id = (idNb?: number): Record<'id', number> => ({ id: idNb || 42 });
+
 const tb = <T extends 'start' | 'end'>(t: T) => (
   target: number,
   minTime?: number
@@ -36,16 +51,39 @@ const tb = <T extends 'start' | 'end'>(t: T) => (
     {}
   );
 };
+
+/**
+ * Construct query's `start` property. Max set to `target`
+ * @param target: target time
+ * @param minTime: minimal time. If not provided, use `target` duration
+ */
 export const start = tb('start');
+
+/**
+ * Construct query's `end` property. Max set to `target`
+ * @param target: target time
+ * @param minTime: minimal time. If not provided, use `target` duration
+ */
 export const end = tb('end');
+/**
+ * Construct `timeDuration` object.
+ * @param target target duration
+ * @param minTime minimal duration. If not provided, use `target` duration.
+ */
 export const timeDuration = (target: number, minTime?: number): ITimeDuration => {
   const min = minTime || target;
   return { min, target };
 };
+
+/**
+ * Construct query's `duration` property
+ * @param dur use `timeDuration` function
+ */
 export const duration = (dur: ITimeDuration): Record<'duration', ITimeDuration> => {
   return assoc('duration', dur, {});
 };
-export const timeRestriction = (
+
+const timeRestriction = (
   condition: RestrictionCondition,
   ranges: ReadonlyArray<[number, number]>
 ) => {
@@ -54,6 +92,10 @@ export const timeRestriction = (
     ranges,
   };
 };
+
+/**
+ * Construct query's `timeRestrictions` property
+ */
 export const timeRestrictions = (
   type: keyof ITimeRestrictions,
   condition: RestrictionCondition,
@@ -63,6 +105,10 @@ export const timeRestrictions = (
     'timeRestrictions',
     ITimeRestrictions
   >;
+
+/**
+ * Construct query's `goal` property
+ */
 export const goal = (
   kindEn: GoalKind,
   quantity: ITimeDuration,
@@ -71,6 +117,11 @@ export const goal = (
   goal: { kind: kindEn, quantity, time },
 });
 
+/**
+ * Merge all partials queries to form one query. Provide default `id`, `name` and `kind` (`IBaseQuery` properties)
+ * @param factories Partial query objects resulting from factories functions
+ * @returns Query object built from merging all partials
+ */
 export const queryFactory = <T extends IQuery>(...factories: Array<Partial<T>>): T => {
   return mergeAll([id(), name(), kind(), ...factories]) as T;
 };
